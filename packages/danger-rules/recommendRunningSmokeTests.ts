9290e9a826b9557d3d693cf00279f6bee97cb6a6
Jahed Ahmed <jahed@snyk.io>
2021-11-17 15:12:57 +0000

    chore: move danger rules a private package
    Makes it easier to manage dependencies specific to it.

---
+++ packages/danger-rules/recommendRunningSmokeTests.ts
@@ -0,0 +1,21 @@
+import { danger, message } from 'danger';
+
+export function recommendRunningSmokeTests(): void {
+  const branchPrefix = 'smoke/';
+  const workflowPath = '.github/workflows/smoke-tests.yml';
+
+  const modifiedSmokeTest =
+    danger.git.modified_files.some((f) => f.startsWith('test/smoke/')) ||
+    danger.git.created_files.some((f) => f.startsWith('test/smoke/')) ||
+    danger.git.modified_files.includes(workflowPath);
+
+  const isOnSmokeTestBranch = danger.github.pr.head.ref.startsWith(
+    branchPrefix,
+  );
+
+  if (modifiedSmokeTest && !isOnSmokeTestBranch) {
+    message(
+      `You are modifying something in test/smoke directory, yet you are not on the branch starting with ${branchPrefix}. You can prefix your branch with ${branchPrefix} and Smoke tests will trigger for this PR.`,
+    );
+  }
+}
---
+++ packages/danger-rules/recommendWritingTest.ts
@@ -0,0 +1,16 @@
+import { danger, warn } from 'danger';
+
+export function recommendWritingTest(): void {
+  const modifiedTest =
+    danger.git.modified_files.some((f) => f.startsWith('test/')) ||
+    danger.git.created_files.some((f) => f.startsWith('test/'));
+  const modifiedSrc =
+    danger.git.modified_files.some((f) => f.startsWith('src/')) ||
+    danger.git.created_files.some((f) => f.startsWith('src/'));
+
+  if (modifiedSrc && !modifiedTest) {
+    warn(
+      "You've modified files in src/ directory, but haven't updated anything in test folder. Is there something that could be tested?",
+    );
+  }
+}
